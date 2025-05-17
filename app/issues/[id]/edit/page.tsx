ad8bbcef644136cf0d4995f7c6b8ac1626680677
Adonay Amar <adonayamar@gmail.com>
2025-05-17 01:57:39 -0400

    Update issues

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -3,7 +3,11 @@ import IssueForm from "../../_components/IssueForm";
 import { prisma } from "@/prisma/client";
 import { notFound } from "next/navigation";
 
-const EditIssuePage = async ({ params }: { params: { id: string } }) => {
+interface Props {
+  params: { id: string };
+}
+
+const EditIssuePage = async ({ params }: Props) => {
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
