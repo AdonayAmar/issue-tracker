f2a18cf242c2740a92d2ccff7bda2494cdb3109d
Adonay Amar <adonayamar@gmail.com>
2025-05-23 02:45:04 -0400

    Added status update menu

--- app/components/IssueStatusBadge.tsx
+++ app/components/IssueStatusBadge.tsx
@@ -1,19 +1,62 @@
+"use client";
 import React from "react";
-import { Status } from "../generated/prisma/client";
-import { Badge } from "@radix-ui/themes";
+import { Issue, Status } from "../generated/prisma/client";
+import { Badge, Select } from "@radix-ui/themes";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 const statusMap: Record<
   Status,
-  { label: string; color: "red" | "violet" | "green" }
+  {
+    label: string;
+    value: "OPEN" | "IN_PROGRESS" | "CLOSED";
+    color: "red" | "violet" | "green";
+  }
 > = {
-  OPEN: { label: "Open", color: "red" },
-  IN_PROGRESS: { label: "In Progress", color: "violet" },
-  CLOSED: { label: "Closed", color: "green" },
+  OPEN: { label: "Open", value: "OPEN", color: "red" },
+  IN_PROGRESS: { label: "In Progress", value: "IN_PROGRESS", color: "violet" },
+  CLOSED: { label: "Closed", value: "CLOSED", color: "green" },
 };
 
-const IssueStatusBadge = ({ status }: { status: Status }) => {
+const statuses = [statusMap.OPEN, statusMap.IN_PROGRESS, statusMap.CLOSED];
+
+interface Props {
+  status: Status;
+  issueId: number;
+}
+
+const IssueStatusBadge = ({ status, issueId }: Props) => {
+  const assignStatus = (status: "OPEN" | "IN_PROGRESS" | "CLOSED") => {
+    console.log(status);
+    axios
+      .patch("/api/issues/" + issueId, {
+        status: status,
+      })
+      .then(() => {
+        toast.success("Issue Updated");
+      })
+      .catch(() => {
+        toast.error("Changes could not be saved.");
+      });
+  };
+
   return (
-    <Badge color={statusMap[status].color}>{statusMap[status].label}</Badge>
+    <>
+      <Select.Root
+        defaultValue={statusMap[status].value}
+        onValueChange={assignStatus}
+      >
+        <Select.Trigger />
+        <Select.Content>
+          {statuses.map((status) => (
+            <Select.Item key={status.label} value={status.value}>
+              <Badge color={status.color}>{status.label}</Badge>
+            </Select.Item>
+          ))}
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
